fc06a28599761556c1233be8bae5ebf1a88ee2a2
Egor Tonkonogov <egtonk@gmail.com>
2017-03-29 22:35:41 +0700

    Изменил цвет гистограммы

--- js/stat.js
+++ js/stat.js
@@ -6,37 +6,38 @@ window.renderStatistics = function (ctx, names, times){
   ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = 'white';
   ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270); //рисую облака
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+  ctx.fillText('Список результатов:', 120, 60); //рисую надписи
 
-  var max = -1;
-  var maxIndex = -1;
+  var max = -1; //задаю переменную наибольшего времени
 
   for (var i = 0 ; i < times.length; i++) {
-    var time = times[i];
+    var time = times[i];  //переменная для поиска времени
     if (time > max) {
       max = time;
-      maxIndex = i;
     }
-  }
+  } //вычисляю наибольшее время
 
-  var histogramWidth = 150;              // px;
-  var step = histogramWidth / max; // px;
+  var histogramHeight = 150; //px
+  var step = histogramHeight / max; //высчитываю величину для нормирования гистограммы
+  var barWidth = 40;  // px;
+  var indent = barWidth + 50; // px;
+  var initialX = 140; // px;
+  var initialY = 240; // px;
 
-  var barHeigth = 20; // px;
-  var indent = 40;    // px;
-  var initialX = 80; // px;
-  var initialY = 120;  // px;
-
-  ctx.textBaseline = 'top'; // положение надписи от левого верхнего угла
   for(var i = 0; i < times.length; i++) {
-    ctx.fillRect(initialX, initialY + indent * i, times[i] * step, barHeigth);
-    ctx.fillText(names[i], initialX + histogramWidth, initialY + indent * i);
-  }
-};
-
+    ctx.fillStyle = 'black';
+    ctx.fillText(times[i].toFixed(0), initialX + indent * i, initialY - (step + 10));  //время игроков
+    ctx.fillText(names[i], initialX + indent * i, initialY + 20); //имена игроков
+      if (names[i] == 'Вы') {
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      } else {
+        ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+      }
+    ctx.fillRect(initialX + indent * i, initialY, barWidth, times[i] * step); //столбцы
+  } //рисую гистограмму
 };
